6e4ee56391705e637db1ea95bc3c039cc5f6f896
hllverel <hllozdemir@live.com>
2025-11-17 13:45:24 +0300

    edited task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,15 +1,14 @@
 `use strict`;
 
 function calcAverageCalories(days) {
+    if (days.length === 0) {
+        return 0;
+  };
     let totalCalories = 0;
     for (const day of days) {
         totalCalories += day.calories;
-    };
-    if (days.length === 0) {
-        return 0;
-    } else {
+  };
         return totalCalories / days.length;
-    }
 }
 
 
